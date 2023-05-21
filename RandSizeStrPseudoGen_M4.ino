825c050f8fa40ea467ce041df1604a74610cd32e
danindiana <danindiana@users.noreply.github.com>
2023-05-21 03:57:42 -0500

    Create RandSizeStrPseudoGen_M4.ino
    Refactor that previous code and set the default size of the random numbers output to vary randomly. Also randomize the default size of the random alphanumeric strings and pseduowords by default. Make sure to provide the option for users to modify the code to output specific output sizes/string lengths in the code.
    
    Here's the refactored code which generates random numbers, alphanumeric strings and pseudowords of random lengths by default. Users can modify the code to generate elements of specific sizes by changing the min and max values for lengths.

---
+++ RandSizeStrPseudoGen_M4.ino
@@ -0,0 +1,58 @@
+#define NUMBER_MIN 1
+#define NUMBER_MAX 999999
+#define LENGTH_MIN 5 // User can modify this value to their preferred minimum string length
+#define LENGTH_MAX 15 // User can modify this value to their preferred maximum string length
+
+const char* consonants = "bcdfghjklmnpqrstvwxyz";
+const char* vowels = "aeiou";
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // Initialize random seed:
+  randomSeed(analogRead(0));
+}
+
+void loop() {
+  Serial.println("Random Number:");
+  Serial.println(random(NUMBER_MIN, NUMBER_MAX + 1));
+  delay(1000);
+
+  int randomStringLength = random(LENGTH_MIN, LENGTH_MAX + 1);
+  Serial.println("Random Alphanumeric String of length " + String(randomStringLength) + ":");
+  Serial.println(generateRandomAlphanumericString(randomStringLength));
+  delay(1000);
+
+  int randomPseudowordLength = random(LENGTH_MIN, LENGTH_MAX + 1);
+  Serial.println("Random Pseudoword of length " + String(randomPseudowordLength) + ":");
+  Serial.println(generatePseudoword(randomPseudowordLength));
+  delay(1000);
+}
+
+String generateRandomAlphanumericString(int length) {
+  String charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  String randomString = "";
+  for (int i = 0; i < length; i++)
+  {
+    byte randomValue = random(0, charset.length());
+    randomString += charset[randomValue];
+  }
+  return randomString;
+}
+
+
+String generatePseudoword(int length) {
+  String word = "";
+  for (int i = 0; i < length; i++)
+  {
+    if (i % 2 == 0)
+      word += consonants[random(0, strlen(consonants) - 1)];
+    else
+      word += vowels[random(0, strlen(vowels) - 1)];
+  }
+  return word;
+}
